e92162adc951e3dc52156ecb05a7d3c2f067e9d3
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-02-11 19:47:48 -0800

    i am in trouble

--- js/app.js
+++ js/app.js
@@ -65,7 +65,7 @@ function fifthQuestion(){
     alert('You are not wrong.');
     correctA++
   } else if(answerFive === 'no' || answerFive === 'n'){
-    alert(`${questionOne}, you are not wrong.`);
+    alert(`you are not wrong.`);
   }
 }
 
@@ -107,93 +107,17 @@ function seventhQuestion(){
     }
   }
   alert(`Here were my possible answers - ${correctNPs}! There is a lot, trust me. `)
-  alert(`you got ${correctA} right!`)
+  alert(`you got ${correctA} out of right!`)
 }
 
 //alert(`enjoy my site ${questionOne}.`)
 
-firstQuestion();
-secondQuestion();
-thirdQuestion();
-fourthQuestion();
-fifthQuestion();
-sixthQuestion();
-seventhQuestion();
+// firstQuestion();
+// secondQuestion();
+// thirdQuestion();
+// fourthQuestion();
+// fifthQuestion();
+// sixthQuestion();
+// seventhQuestion();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementById('showImage').onclick = function () {
-//   document.getElementById('Seattle.jpeg').style.visibility = "visible";
-
-// }
-
-// let attempts = 4;
-// let answer = 10; 
-
-// while(attempts){
-//   attempts--;
-//   let userguess = prompt('guess a number');
-
-//   if(+userguess=== answer){ //+makesthe userguess = a integer.
-//     alert('you did it.');
-//     //break; or
-//     attempts = 0;
-//   } else if (userguess > answer){
-//     alert('too high');
-//   } else {
-//     alert('too low');
-//   } if(attempts === 0){
-//     alert('the correct answer is 10');
-//   }
-// } 
-
-// let myArr = ['pink', 'navy', 'red'];
-// // 6 attempts
-
-// let newattempts = 6;
-// let correctAns = false;
-
-// while(newattempts && !correctAns){ // slow loop first guess
-//   newattempts--;
-//   let userguess = prompt('guess a color');
-
-//   for(let i = 0; i < myArr.length; i++){ // fast loop quickly go through the arreay to check if guess is in the array
-//     if(colorguess === myArr[i]){
-//       alert('yup!');
-//       newattempts = 1;
-//     }
-
-//   }
-// }   
